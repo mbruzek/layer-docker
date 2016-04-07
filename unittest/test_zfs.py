924f4749f8644a16cebc5904e0718c85a54c4d5c
Matt Bruzek <matthew.bruzek@canonical.com>
2016-04-07 10:56:34 -0500

    New tests closer to how this will be used.

--- unittest/test_zfs.py
+++ unittest/test_zfs.py
@@ -21,7 +21,7 @@ class TestZfs(unittest.TestCase):
         print('Creating files in {0}'.format(self.directory))
         # The command to create multiple images to mount zfs.
         image = 'dd if=/dev/zero of={0} bs=1M count=150'
-        for a in range(3):
+        for a in range(6):
             output_file = '{0}/zfs{1}.img'.format(self.directory, str(a))
             print(image.format(output_file))
             # Create the files
@@ -32,13 +32,16 @@ class TestZfs(unittest.TestCase):
     def test_adding(self):
         '''Test the init method of the ZfsPool.'''
         pool_name = 'test-zfs-pool-1'
+        # Create zfs pool by name.
         pool = ZfsPool(pool_name)
-        for a in range(3):
-            print(self.devices[a])
-            pool.add([self.devices[a]])
+        print('Adding the first 3 devices.')
+        pool.add(self.devices[0:3])
+        print('Adding the last 3 devices.')
+        pool.add(self.devices[3:6])
         # Run a command that lists the zfs pool pool.
         cmd = 'sudo zpool list -H {0}'.format(pool_name)
         output = check_output(split(cmd))
+        print(output)
         destroy = 'sudo zpool destroy -f {0}'.format(pool_name)
         check_call(split(destroy))
         print('Pool {0} destroyed.'.format(pool_name))
@@ -46,10 +49,14 @@ class TestZfs(unittest.TestCase):
 
     def test_create(self):
         '''Test the create model of the ZfsPool.'''
-        pool = ZfsPool.create(self.mount_point, self.devices)
+        print('Creating a pool with the first 3 devices.')
+        pool = ZfsPool.create(self.mount_point, self.devices[0:3])
         pool_name = pool.pool_name
+        print('Adding 3 more devices')
+        pool.add(self.devices[3:6])
         cmd = 'sudo zpool list -H {0}'.format(pool_name)
         output = check_output(split(cmd))
+        print(output)
         destroy = 'sudo zpool destroy -f {0}'.format(pool_name)
         check_call(split(destroy))
         print('Pool {0} destroyed.'.format(pool_name))
